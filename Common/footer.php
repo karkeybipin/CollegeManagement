137d93296231546c5a6cc41c155a90b7b613db32
Bipin karki <karkeybipin@gmail.com>
2024-09-09 11:14:03 +0545

    Make the student and teachers to login and handles the data - On progress

---
+++ Common/footer.php
@@ -0,0 +1,45 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>  College</h3>
+            <p>This Project has been made by Muhammad Husnain Raza with the help of 
+              my Allah Almighty.
+              The prupose of this project is Learning Computer Languages.
+              I hope you realy enjoyed to visit this project. Thanks!
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.Sc. CSIT</a></li>
+              <li><a href="">BCA</a></li>
+              <li><a href="">BBM</a></li>
+              <li><a href="">BBS</a></li>
+              <li><a href="">BASW</a></li>
+              <li><a href="">BSW</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address:   College of It and business studies,
+                  (Town),  (Chowk), (Road-map),(code)</p>
+              <p>Telephones: Number...</p>
+              <p>E-mail: college@management.edu.np</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights © reserved by Bipin karki</p>
+</footer>
